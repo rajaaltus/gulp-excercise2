e78f9614409b5962f57993aacfe6f161d7989038
Raja S <rajas@Rajas-MacBook-2.local>
2017-10-26 10:55:14 +0700

    animation added to site

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,11 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
+//Javascript Mobile Menu
 var mobileMenu = new MobileMenu();
+
+//javascript reveal on scoll for sections
+new RevealOnScroll($(".icon-section__section-block"), "55%");
+new RevealOnScroll($(".two-column-section"), "55%");
+new RevealOnScroll($(".newsletter"), "55%");
